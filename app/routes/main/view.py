10439abea0a349ef3ac6784fb4c8ed3aa2abb5fe
Соколов Данила <2000akula@mail.ru>
2022-08-16 08:02:30 +0300

    Added static image && Jinja template
    Show how to add static files for project and how send and get data from python

--- app/routes/main/view.py
+++ app/routes/main/view.py
@@ -1,6 +1,9 @@
+from flask import render_template
+
 from app.routes.main import bp
 
 
 @bp.route('/', methods=['GET', 'POST'])
 def main():
-    return "Hello world"
+    colors = ['Black', 'Blue', 'Purple']
+    return render_template('base.html', colors=colors)
